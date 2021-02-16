618a54ab2c6271751908066fba2e9b1ca7a6579b
iavn666 <dvoeglazovcom@gmail.com>
2021-02-16 14:11:28 +0500

    5.2.1. Реализовать SimpleArray<T> [#156][#473177]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -1,27 +1,41 @@
 package ru.job4j.generics;
 
 
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Objects;
 
-public class SimpleArray<T> implements Iterable<T> {
-
-    private int count = 0;
+public class SimpleArray<T> implements Iterator<T> {
 
     public SimpleArray[] myArray = new SimpleArray[10];
 
-    public void add(T model) {
-        myArray[count++] = (SimpleArray) model;
+    public T add(T model) {
+        SimpleArray[] result = new SimpleArray[10];
+        for (int index = 0; index <= myArray.length - 1; index++) {
+            if (myArray[index] == null) {
+                myArray[index] = (SimpleArray) model;
+            }
+            result = myArray;
+
+        }
+        return (T) result;
     }
 
+    public T set(int index, T model) {
+        SimpleArray[] result = new SimpleArray[10];
+        for (int i = 0; i <= myArray.length - 1; index++) {
+            if(myArray[i] == myArray[index]) {
+                myArray[i] = (SimpleArray) model;
+            }
+            result = myArray;
+        }
 
-    public void set(int index, T model) {
-        myArray[index] = (SimpleArray) model;
+        return (T) result;
     }
 
-    public void remove(int index) {
+    public T remove(int index) {
         SimpleArray[] result = new SimpleArray[myArray.length - 1];
         System.arraycopy(myArray, index + 1, result, index, myArray.length - 1);
+        return (T) result;
     }
 
     public T get(int index) {
@@ -35,23 +49,16 @@ public class SimpleArray<T> implements Iterable<T> {
 
     }
 
+    private int pos=0;
+
+
     @Override
-    public Iterator<T> iterator() {
-        Iterator<T> num = new Iterator<T>() {
-
-            @Override
-            public boolean hasNext() {
-                return count< myArray.length;
-            }
-
-            @Override
-            public T next() {
-                return (T) myArray[count++];
-            }
-
-        };
-        return num;
+    public boolean hasNext() {
+           return myArray.length>pos;
     }
 
-
+    @Override
+    public T next() {
+        return (T) myArray[pos++];
+    }
 }
