f7e0ed9f378ebac06c0b7055c609480383610691
iavn666 <dvoeglazovcom@gmail.com>
2021-02-18 18:59:28 +0500

    5.2.1. Реализовать SimpleArray<T> [#156][#473177]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -8,29 +8,33 @@ public class SimpleArray<T> implements Iterable<T> {
 
     private int count = 0;
 
-    public SimpleArray[] myArray = new SimpleArray[10];
+    private T[] myArray = (T[]) new Object[10];
 
     public void add(T model) {
-        myArray[count++] = (SimpleArray) model;
-        }
+        myArray[count++] = model;
+    }
+
 
     public void set(int index, T model) {
-        myArray[index] = (SimpleArray) model;
-        }
+        Objects.checkIndex(index, myArray.length);
+        myArray[index] = model;
+    }
 
     public void remove(int index) {
-        SimpleArray[] result = new SimpleArray[myArray.length - 1];
+        Objects.checkIndex(index, myArray.length);
+        T[] result = (T[]) new Object[myArray.length - 1];
         System.arraycopy(myArray, index + 1, result, index, myArray.length - 1);
-        }
+    }
 
     public T get(int index) {
-        SimpleArray result = null;
+        Objects.checkIndex(index, myArray.length);
+        T result = null;
         for (int i = 0; i <= myArray.length - 1; i++ ) {
             if (myArray[i] == myArray[index]) {
                 result = myArray[i];
             }
         }
-        return (T) result;
+        return result;
 
     }
 
